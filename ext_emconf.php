1251df30af29f76d3d3c9f142ef6b1e7bdc960bb
enobe.de <enobe.de@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-09-21 05:53:02 +0000

    Cleanup: Upgraded ext_emconf.php
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/eepcollect/trunk@38380 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "eepcollect"
+# Extension Manager/Repository config file for ext "eepcollect".
 #
-# Auto generated 12-09-2010 22:31
+# Auto generated 21-09-2010 07:39
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Visitors can collect pages and add them to their own favorit-list like a basket in a shop. Inclusiv delete and sort functions. (Merkliste, Merkzettel, Seite merken, Inhalte sammeln -> http://pagecollector.enobe.de)',
 	'category' => 'fe',
 	'shy' => 0,
-	'version' => '1.0.8',
+	'version' => '1.0.9',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -40,7 +40,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:34:{s:9:"ChangeLog";s:4:"6edf";s:12:"ext_icon.gif";s:4:"06da";s:17:"ext_localconf.php";s:4:"2d14";s:14:"ext_tables.php";s:4:"394c";s:14:"ext_tables.sql";s:4:"02be";s:19:"flexform_ds_pi1.xml";s:4:"cb6e";s:31:"icon_tx_eepcollect_sessions.gif";s:4:"d49a";s:28:"locallang_csh_eepcollect.php";s:4:"c6e1";s:16:"locallang_db.xml";s:4:"dbd7";s:17:"locallang_tca.xml";s:4:"6aae";s:7:"tca.php";s:4:"737e";s:14:"doc/manual.pdf";s:4:"fc67";s:14:"doc/manual.sxw";s:4:"7505";s:17:"doc/manual_de.pdf";s:4:"8f7d";s:17:"doc/manual_de.sxw";s:4:"273a";s:31:"pi1/class.tx_eepcollect_pi1.php";s:4:"751a";s:17:"pi1/locallang.xml";s:4:"3b80";s:24:"pi1/static/editorcfg.txt";s:4:"0858";s:23:"res/bigbutton_minus.gif";s:4:"2c7e";s:22:"res/bigbutton_okay.gif";s:4:"8d76";s:22:"res/bigbutton_plus.gif";s:4:"4068";s:20:"res/button_blank.gif";s:4:"82a2";s:19:"res/button_down.gif";s:4:"6267";s:28:"res/button_down_disabled.gif";s:4:"a7e0";s:20:"res/button_minus.gif";s:4:"c874";s:19:"res/button_plus.gif";s:4:"3470";s:17:"res/button_up.gif";s:4:"4f2a";s:26:"res/button_up_disabled.gif";s:4:"f689";s:20:"static/constants.txt";s:4:"375d";s:16:"static/setup.txt";s:4:"a709";s:28:"template/eepcollect_pi1.tmpl";s:4:"efd3";s:43:"template/eepcollect_powermail_checkbox.tmpl";s:4:"9a09";s:41:"template/eepcollect_powermail_select.tmpl";s:4:"ef10";s:25:"template/no_template.tmpl";s:4:"787c";}',
+	'_md5_values_when_last_written' => 'a:35:{s:9:"ChangeLog";s:4:"7944";s:12:"ext_icon.gif";s:4:"06da";s:17:"ext_localconf.php";s:4:"2d14";s:14:"ext_tables.php";s:4:"394c";s:14:"ext_tables.sql";s:4:"02be";s:19:"flexform_ds_pi1.xml";s:4:"cb6e";s:31:"icon_tx_eepcollect_sessions.gif";s:4:"d49a";s:28:"locallang_csh_eepcollect.php";s:4:"c6e1";s:16:"locallang_db.xml";s:4:"dbd7";s:17:"locallang_tca.xml";s:4:"6aae";s:7:"tca.php";s:4:"737e";s:14:"doc/manual.pdf";s:4:"fc67";s:14:"doc/manual.sxw";s:4:"7505";s:17:"doc/manual_de.pdf";s:4:"8f7d";s:17:"doc/manual_de.sxw";s:4:"273a";s:31:"pi1/class.tx_eepcollect_pi1.php";s:4:"fcc3";s:17:"pi1/locallang.xml";s:4:"3b80";s:24:"pi1/static/editorcfg.txt";s:4:"0858";s:16:"res/_buttons.psd";s:4:"bab9";s:23:"res/bigbutton_minus.gif";s:4:"2c7e";s:22:"res/bigbutton_okay.gif";s:4:"8d76";s:22:"res/bigbutton_plus.gif";s:4:"4068";s:20:"res/button_blank.gif";s:4:"82a2";s:19:"res/button_down.gif";s:4:"6267";s:28:"res/button_down_disabled.gif";s:4:"a7e0";s:20:"res/button_minus.gif";s:4:"c874";s:19:"res/button_plus.gif";s:4:"3470";s:17:"res/button_up.gif";s:4:"4f2a";s:26:"res/button_up_disabled.gif";s:4:"f689";s:20:"static/constants.txt";s:4:"8961";s:16:"static/setup.txt";s:4:"a709";s:28:"template/eepcollect_pi1.tmpl";s:4:"efd3";s:43:"template/eepcollect_powermail_checkbox.tmpl";s:4:"9a09";s:41:"template/eepcollect_powermail_select.tmpl";s:4:"ef10";s:25:"template/no_template.tmpl";s:4:"787c";}',
+	'suggests' => array(
+	),
 );
 
 ?>
